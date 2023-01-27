172c50e32ee2bd28a61592b1a2e815f8144e707f
fyesidospina <arquitcode@gmail.com>
2023-01-27 12:03:19 -0500

    pom api

--- src/main/java/com/apispring/api.java
+++ src/main/java/com/apispring/api.java
@@ -1,34 +1,13 @@
-package src.main.java.com.apispring;
+package com.apispring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class api {
 
-    public static void main(String[] args) {
-        SpringApplication.run(api.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(api.class, args);
+	}
 
-    @RequestMapping("/")
-    @ResponseBody
-    public Message displayMessage() {
-        return new Message();
-    }
-
-    static class Message {
-        private String content = "Greetings!";
-
-        public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-    }
 }
