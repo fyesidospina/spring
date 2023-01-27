a78ff83f3353f1d30c117561a741c64e5d2b4e70
fyesidospina <arquitcode@gmail.com>
2023-01-27 12:14:41 -0500

    new pom

--- src/main/java/com/api/spring/Application.java
+++ src/main/java/com/api/spring/Application.java
@@ -1,13 +1,13 @@
-package com.apispring;
+package com.api.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class api {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(api.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
